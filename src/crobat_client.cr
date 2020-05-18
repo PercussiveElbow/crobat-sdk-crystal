b999c3c3187a7d8acd3a32a5d703397ddf72c39a
PercussiveElbow <PercussiveElbow@users.noreply.github.com>
2020-05-19 00:27:58 +0100

    Splitting SDK code out

--- src/crobat_client.cr
+++ src/crobat_client.cr
@@ -1,92 +1,8 @@
 require "http/client"
 require "json"
 require "option_parser"
+require "./crobat_sdk"
 
-VERSION = "0.1.0"
-
-class CrobatClient
-
-  def initialize(@api_url : String)
-    if @api_url.includes?("https://")  && @api_url.includes?("http://")
-      raise CrobatInvalidAPIConfigException.new("#{@api_url} is not a valid API")
-    end
-  end
-
-  def retrieve_subdomains(domain : String, page : Int) : Array(String)
-    begin
-      page_query = page > 0 ? "?page=#{page.to_s}" : ""
-      resp = HTTP::Client.get("#{@api_url}/subdomains/#{domain}#{page_query}")
-      if resp.status_code == 200
-        items = Array(String).from_json(resp.body)
-        return items.size()>=999 ? items + retrieve_subdomains(domain,page+1) : items
-      else
-        raise CrobatQueryException.new("Error querying Subdomains API Endpoint #{@api_url} with domain #{domain}. Status code #{resp.status_code}")
-      end
-    rescue ex : Exception
-      print(ex)
-      return [] of String
-    end
-  end
-
-  def retrieve_tlds(search_query : String, page : Int) : Array(String)
-    begin
-      page_query = page > 0 ? "?page=#{page.to_s}" : ""
-      resp = HTTP::Client.get("#{@api_url}/tlds/#{search_query}#{page_query}")
-      if resp.status_code == 200
-        items = Array(String).from_json(resp.body)
-        return items.size()>=999 ? items + retrieve_tlds(search_query,page+1) : items
-      else
-        raise CrobatQueryException.new("Error querying TLD API Endpoint #{@api_url} with TLD #{search_query}. Status code #{resp.status_code}")
-      end
-    rescue ex : Exception
-      print(ex)
-      return [] of String
-    end
-  end
-
-  def retrieve_all(search_query : String, page : Int) : Array(SonarAllResult)
-    begin
-      page_query = page > 0 ? "?page=#{page.to_s}" : ""
-      resp = HTTP::Client.get( "#{@api_url}/all/#{search_query}#{page_query}")
-      if resp.status_code == 200
-        items = Array(SonarAllResult).from_json(resp.body)
-        if items.size()>=999
-          recursive_items = retrieve_all(search_query,page+1)
-          return recursive_items.size()> 0 ? items + recursive_items :  items
-        else
-          return items
-        end
-      else
-        puts(resp.headers)
-        raise CrobatQueryException.new("Error querying ALL API Endpoint #{@api_url} with query #{search_query}. Status code #{resp.status_code}")
-        return [] of SonarAllResult
-      end
-    rescue ex : Exception
-      print(ex)
-      return [] of SonarAllResult
-    end
-  end
-
-  class SonarAllResult 
-    JSON.mapping(
-      name: String,
-      subdomain: String,
-      tld: String,
-      value: String,
-      type: String
-    )
-
-    def to_s
-      "#{self.name} #{self.subdomain} #{self.tld} #{self.value} #{self.type}"
-    end
-  end
-
-  class CrobatQueryException < Exception
-  end
-  class CrobatInvalidAPIConfigException < Exception
-  end
-
-end
 
 def output_txt_or_csv(output_results : Array(String), output_path : String, output_file)
   output_results.each do | result |
@@ -109,7 +25,7 @@ def output_to_file(results, output_format : String, output_path : String, search
         puts("Unable to output file")
     end
 
-  elsif results.is_a?(Array(CrobatClient::SonarAllResult))
+  elsif results.is_a?(Array(Crobat::CrobatSDK::SonarAllResult))
     organised_results = [] of String
     results.each do | result | 
       organised_results.push(result.name + ", " + result.subdomain + ", " + result.tld  + ", " + result.value + ", " + result.type)
@@ -149,7 +65,7 @@ def cli
     puts("No domain and search type supplied. Check -h for help")
     exit(1)
   else
-    client = CrobatClient.new("https://sonar.omnisint.io")
+    client = Crobat::CrobatSDK.new("https://sonar.omnisint.io")
     case search_type.downcase
     when "all"
       results = client.retrieve_all(target, 0)
---
+++ src/crobat_sdk.cr
@@ -0,0 +1,86 @@
+
+module Crobat
+    class CrobatSDK
+
+        def initialize(@api_url : String)
+        if @api_url.includes?("https://")  && @api_url.includes?("http://")
+            raise CrobatInvalidAPIConfigException.new("#{@api_url} is not a valid API")
+        end
+        end
+    
+        def retrieve_subdomains(domain : String, page : Int) : Array(String)
+        begin
+            page_query = page > 0 ? "?page=#{page.to_s}" : ""
+            resp = HTTP::Client.get("#{@api_url}/subdomains/#{domain}#{page_query}")
+            if resp.status_code == 200
+            items = Array(String).from_json(resp.body)
+            return items.size()>=999 ? items + retrieve_subdomains(domain,page+1) : items
+            else
+            raise CrobatQueryException.new("Error querying Subdomains API Endpoint #{@api_url} with domain #{domain}. Status code #{resp.status_code}")
+            end
+        rescue ex : Exception
+            print(ex)
+            return [] of String
+        end
+        end
+    
+        def retrieve_tlds(search_query : String, page : Int) : Array(String)
+        begin
+            page_query = page > 0 ? "?page=#{page.to_s}" : ""
+            resp = HTTP::Client.get("#{@api_url}/tlds/#{search_query}#{page_query}")
+            if resp.status_code == 200
+            items = Array(String).from_json(resp.body)
+            return items.size()>=999 ? items + retrieve_tlds(search_query,page+1) : items
+            else
+            raise CrobatQueryException.new("Error querying TLD API Endpoint #{@api_url} with TLD #{search_query}. Status code #{resp.status_code}")
+            end
+        rescue ex : Exception
+            print(ex)
+            return [] of String
+        end
+        end
+    
+        def retrieve_all(search_query : String, page : Int) : Array(SonarAllResult)
+        begin
+            page_query = page > 0 ? "?page=#{page.to_s}" : ""
+            resp = HTTP::Client.get( "#{@api_url}/all/#{search_query}#{page_query}")
+            if resp.status_code == 200
+            items = Array(SonarAllResult).from_json(resp.body)
+            if items.size()>=999
+                recursive_items = retrieve_all(search_query,page+1)
+                return recursive_items.size()> 0 ? items + recursive_items :  items
+            else
+                return items
+            end
+            else
+            puts(resp.headers)
+            raise CrobatQueryException.new("Error querying ALL API Endpoint #{@api_url} with query #{search_query}. Status code #{resp.status_code}")
+            return [] of SonarAllResult
+            end
+        rescue ex : Exception
+            print(ex)
+            return [] of SonarAllResult
+        end
+        end
+    
+        class SonarAllResult 
+        JSON.mapping(
+            name: String,
+            subdomain: String,
+            tld: String,
+            value: String,
+            type: String
+        )
+    
+        def to_s
+            "#{self.name} #{self.subdomain} #{self.tld} #{self.value} #{self.type}"
+        end
+        end
+    
+        class CrobatQueryException < Exception
+        end
+        class CrobatInvalidAPIConfigException < Exception
+        end
+    
+    end
+end
